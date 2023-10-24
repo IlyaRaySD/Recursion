9a9363097e163eea809bee1965c59eebb1952286
IlyaRaySD <xray2003jy@gmail.com>
2023-10-24 15:06:02 +0300

    Working with the Fibonacci sequence

--- Recursion/Recursion.cpp
+++ Recursion/Recursion.cpp
@@ -2,6 +2,33 @@
 
 using namespace std;
 
-int main() {
+int fibonachi(int num) {
+	if (num == 1 or num == 2)
+		return 1;
+	if (num < 1)
+		return 0;
+	else
+		return (fibonachi(num - 1) + fibonachi(num - 2));
+}
 
+void fibon_one_el() {
+	int n;
+	cout << "Введите порядковый номер элемнта последовательности : ";
+	cin >> n;
+	printf("%d-й элемент: %zu", n, fibonachi(n));
+}
+
+void fibon_sub() {
+	int n;
+	cout << "Введите порядковый номер конечного элемента последовательности: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cout << fibonachi(i) << " ";
+	cout << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	fibon_one_el();
+	fibon_sub();
 }
